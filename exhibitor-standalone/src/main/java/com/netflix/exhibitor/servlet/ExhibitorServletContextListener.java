4242063c473f28e9d2818af5da348443ac26bc8d
Jonathan Pearlin <jonathan.pearlin@dealer.com>
2013-08-07 12:12:22 -0400

    Added remote auth for Tomcat war version.
    Added missing required dependencies.

--- exhibitor-standalone/src/main/java/com/netflix/exhibitor/servlet/ExhibitorServletContextListener.java
+++ exhibitor-standalone/src/main/java/com/netflix/exhibitor/servlet/ExhibitorServletContextListener.java
@@ -16,15 +16,22 @@
 
 package com.netflix.exhibitor.servlet;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.io.Closeables;
 import com.google.common.io.Resources;
 import com.netflix.exhibitor.core.Exhibitor;
+import com.netflix.exhibitor.core.ExhibitorArguments;
+import com.netflix.exhibitor.core.RemoteConnectionConfiguration;
 import com.netflix.exhibitor.standalone.ExhibitorCLI;
 import com.netflix.exhibitor.standalone.ExhibitorCreator;
 import com.netflix.exhibitor.standalone.ExhibitorCreatorExit;
 import com.netflix.exhibitor.standalone.MissingConfigurationTypeException;
+import com.sun.jersey.api.client.filter.ClientFilter;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.api.client.filter.HTTPDigestAuthFilter;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContextEvent;
@@ -33,6 +40,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
@@ -47,6 +55,7 @@ public class ExhibitorServletContextListener implements ServletContextListener
 
     private static final String OUR_PREFIX = "exhibitor-";
     private static final String EXHIBITOR_PROPERTIES = "exhibitor.properties";
+	private static final String SYSTEM_ARG_NAME_FORMAT_STRING = "-%s";
 
     @Override
     public void contextInitialized(ServletContextEvent event)
@@ -57,7 +66,7 @@ public class ExhibitorServletContextListener implements ServletContextListener
         {
             exhibitorCreator = new ExhibitorCreator(toArgsArray(argsBuilder));
 
-            exhibitor = new Exhibitor(exhibitorCreator.getConfigProvider(), null, exhibitorCreator.getBackupProvider(), exhibitorCreator.getBuilder().build());
+            exhibitor = new Exhibitor(exhibitorCreator.getConfigProvider(), null, exhibitorCreator.getBackupProvider(), addRemoteAuth(exhibitorCreator.getBuilder(), argsBuilder.get("--" + ExhibitorCLI.REMOTE_CLIENT_AUTHORIZATION)).build());
             exhibitor.start();
 
             event.getServletContext().setAttribute(ExhibitorServletContextListener.class.getName(), exhibitor);
@@ -152,15 +161,40 @@ public class ExhibitorServletContextListener implements ServletContextListener
         Enumeration parameterNames = properties.propertyNames();
         while ( parameterNames.hasMoreElements() )
         {
-            String  name = String.valueOf(parameterNames.nextElement());
+            final String  name = String.valueOf(parameterNames.nextElement());
             if ( name.startsWith(OUR_PREFIX) )
             {
-                String      value = properties.getProperty(name);
-                String      argName = name.substring(OUR_PREFIX.length());
-                argsBuilder.put("-" + argName, value);
+                final String value = properties.getProperty(name);
+                final String argName = String.format(SYSTEM_ARG_NAME_FORMAT_STRING, name.substring(OUR_PREFIX.length()));
+                argsBuilder.put(argName, value);
 
                 log.info(String.format("Setting property %s=%s", argName, value));
             }
         }
+    }
+
+    private ExhibitorArguments.Builder addRemoteAuth(final ExhibitorArguments.Builder builder, final String remoteAuthSpec) {
+		if(remoteAuthSpec != null && remoteAuthSpec.length() > 0) {
+        	final String[] parts = remoteAuthSpec.split(":");
+        	Preconditions.checkArgument(parts.length == 2, "Badly formed remote client authorization: {}.", remoteAuthSpec);
+
+        	final String type = parts[0].trim();
+        	final String auth = Preconditions.checkNotNull(new String(Base64.decodeBase64(parts[1].trim())), "Authentication token cannot be null.");
+			final String[] authParts = auth.split(":");
+			
+			final String username = authParts[0].trim();
+        	final String password = authParts[1].trim();
+
+        	if ( type.equals("basic") ) {
+				final ClientFilter filter = new HTTPBasicAuthFilter(username, password);
+            	builder.remoteConnectionConfiguration(new RemoteConnectionConfiguration(Arrays.asList(filter)));
+        	} else if ( type.equals("digest") ) {
+				final ClientFilter filter = new HTTPDigestAuthFilter(username, password);
+            	builder.remoteConnectionConfiguration(new RemoteConnectionConfiguration(Arrays.asList(filter)));
+        	} else {
+            	log.warn("Unknown remote client authorization type: {}.", type);
+        	}
+		}
+		return builder;
     }
 }
